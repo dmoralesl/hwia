38b2ca6fb5966ff94ae44abc783217ab466ab31d
David Morales <5dmorales474@gmail.com>
2021-11-19 14:54:56 +0100

    Removing static env files

---
+++ src/environments/set-env.ts
@@ -0,0 +1,35 @@
+const setEnv = () => {
+    const fs = require('fs');
+    const writeFile = fs.writeFile;
+  // Configure Angular `environment.ts` file path
+    const targetPath = './src/environments/environment.ts';
+  // Load node modules
+    const colors = require('colors');
+  // `environment.ts` file structure
+    const envConfigFile = `export const environment = {
+    firebase: {
+        apiKey: '${process.env["FIREBASE_API_KEY"]}',
+        authDomain: '${process.env["FIREBASE_AUTH_DOMAIN"]}',
+        databaseURL: '${process.env["FIREBASE_DB_URL"]}',
+        projectId: '${process.env["FIREBASE_PROJECT_ID"]}',
+        storageBucket: '${process.env["FIREBASE_STORAGE_BUCKET"]}',
+        messagingSenderId: '${process.env["FIREBASE_MESSAGING_SENDER_ID"]}',
+        appId: '${process.env["FIREBASE_API_ID"]}',
+        measurementId: '${process.env["FIREBASE_MEASURAMENT_ID"]}'
+    },
+    production: true,
+  };
+  `;
+    console.log(colors.magenta('The file `environment.ts` will be written with the following content: \n'));
+    writeFile(targetPath, envConfigFile, (err: any) => {
+      if (err) {
+        console.error(err);
+        throw err;
+      } else {
+        console.log(colors.magenta(`Angular environment.ts file generated correctly at ${targetPath} \n`));
+      }
+    });
+  };
+  
+  setEnv();
+  
