138e3d2fea079a780a8c1c0eb795084cc15b5325
David Morales <5dmorales474@gmail.com>
2021-11-28 20:18:58 +0100

    WIP - Dashboard and improving header

---
+++ src/app/helpers.ts
@@ -0,0 +1,42 @@
+import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
+import { DEFAULT_COIN, SECONDS_IN_YEAR } from './constants';
+
+import { Cashify } from 'cashify';
+
+export const getMoneyPerSecond = (money: number, coin?: string): number => {
+  if (!coin) {
+    return money / SECONDS_IN_YEAR;
+  }
+
+  return 0;
+};
+
+export const calculateMoneySince = (
+  moneyPerSecond: number,
+  seconds: number,
+  factor: number
+): number => {
+  return Math.round(moneyPerSecond * seconds * factor);
+};
+
+export const _filter = (name: string, target: Array<any>): Array<any> => {
+  const filterValue = name.toLowerCase();
+  return target.filter((option) =>
+    option.name.toLowerCase().includes(filterValue)
+  );
+};
+
+export const _filterCurrencies = (name: string, target: Array<any>): Array<any> => {
+  const filterValue = name.toLowerCase();
+  return target.filter((option) =>
+    option.displayName.toLowerCase().includes(filterValue))
+};
+
+
+
+export const checkPasswords: ValidatorFn = (group: AbstractControl):  ValidationErrors | null => { 
+  const pass: string | null = group.get('password')?.value;
+  const passRe: string | null = group.get('passwordRe')?.value;
+
+  return pass === passRe ? null : { notSame: true };
+}
